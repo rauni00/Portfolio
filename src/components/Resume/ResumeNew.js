b25b263cb2ffbb3ec6bc29c66d029b1c972fca50
rahul.rauniyar <rahul.rauniyar@otssolutions.com>
2023-05-19 16:13:48 +0530

    dd

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,8 +9,6 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink = "";
-
-
 // const resumeLink =
 //   "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
 
