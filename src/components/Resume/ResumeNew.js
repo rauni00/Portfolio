ff33a3dc5229523886ede5748c8e76490abde1b9
rahul.rauniyar <rahul.rauniyar@otssolutions.com>
2023-05-19 16:09:06 +0530

    first commit

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -10,6 +10,7 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 
 const resumeLink = "";
 
+
 // const resumeLink =
 //   "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
 
